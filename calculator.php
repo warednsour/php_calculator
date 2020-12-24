3d37ebb58f381a981cf81175570fb8de201744c7
warednsour <warednsour@gmail.com>
2020-12-24 12:54:44 +0200

    added the files

---
+++ calculator.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <form action="POST" action="result.php">
+      <input type="text" name="number1"/>
+      <input type="text" name="number2"/>
+      <select class="" name="operation">
+        <option value="plus">сложение</option>
+        <option value="minus">вычитание</option>
+        <option value="omnoshenia">умножение</option>
+        <option value="delenia">деление</option>
+      </select>
+      <input type="submit" name="" value="">
+    </form>
+  </body>
+</html>
---
+++ result.php
@@ -0,0 +1,35 @@
+<?php
+
+$number1 = $_POST['number1'];
+$number2 = $_POST['number2'];
+$plus = $_POST['plus'];
+$minus = $_POST['minus'];
+$omnoshenia = $_POST['omnoshenia'];
+$delenia = $_POST['delenia'];
+$operation = $_POST['operation'];
+
+
+  if(isset($_POST['submit']))
+  {
+    if(is_numeric($number1) && is_numeric($number2)){
+
+      if($operation == $plus) {
+        $result = $number1 + $number2;
+      }
+      if($operation == $minus){
+        $result = $number1 - $number2;
+      }
+      if($operation == $delenia){
+        $result = $number1 / $number2
+      }
+      if($operation == $omnoshenia){
+        $result = $number1 * $number2
+      }
+
+
+
+    }
+            echo '<p> the result is' . $result . '</p>'  ;
+  }
+
+ ?>
